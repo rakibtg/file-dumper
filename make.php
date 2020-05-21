87b6658456c3fe2e2b9e0877f6803636d4730528
rakibtg <rakibtg@gmail.com>
2020-05-21 14:01:07 +0600

    Updated with max file size feature

--- make.php
+++ make.php
@@ -6,6 +6,7 @@
     protected $file;
     protected $expectedFileSize;
     protected $expectedFileSizeUnit;
+    protected $totalLinesWritten;
     protected $totalBytesWritten;
 
     function __construct ($file, $fileSize, $fileSizeUnit) {
@@ -14,6 +15,7 @@
       $this->expectedFileSize = $fileSize;
       $this->expectedFileSizeUnit = $fileSizeUnit;
       $this->totalBytesWritten = 0;
+      $this->totalLinesWritten = 0;
     }
     function row () {
       $row = [
@@ -37,36 +39,57 @@
       ];
       return json_encode($row);
     }
-    function fileSize ($bytes, $decimals = 0) {
-      // $bytes = filesize($this->file);
-      $size = array('B','KB','MB','GB','TB','PB','EB','ZB','YB');
-      $factor = floor((strlen($bytes) - 1) / 3);
-      $sizeAmount = sprintf("%.{$decimals}f", $bytes / pow(1024, $factor));
-      $sizeUnit = preg_replace("/[^A-Z]+/", "", (sprintf("%.{$decimals}f", $bytes / pow(1024, $factor)) . @$size[$factor]));
-      echo $sizeAmount . $sizeUnit . "\n";
-      if ($sizeAmount <= $this->expectedFileSize) {
-        if ($sizeUnit == $this->expectedFileSizeUnit) {
-          return false;
-        } else {
-          return true;
-        }
+    function formatSizeUnits ($bytes) {
+      if ($bytes >= 1073741824) {
+        $bytes = number_format($bytes / 1073741824, 2) . ' GB';
+      } elseif ($bytes >= 1048576) {
+        $bytes = number_format($bytes / 1048576, 2) . ' MB';
+      } elseif ($bytes >= 1024) {
+        $bytes = number_format($bytes / 1024, 2) . ' KB';
+      } elseif ($bytes > 1) {
+        $bytes = $bytes . ' bytes';
+      } elseif ($bytes == 1) {
+        $bytes = $bytes . ' byte';
       } else {
-        return true;
+        $bytes = '0 bytes';
       }
+      return $bytes;
+    }
+    function keepWriting () {
+      [ $unitValue, $unitType ] = explode(" ", $this->formatSizeUnits($this->totalBytesWritten));
+      $unitType = strtolower(trim($unitType));
+      $unitValue = (int) $unitValue;
+      if ($unitType === strtolower($this->expectedFileSizeUnit)) {
+        if ($unitValue >= $this->expectedFileSize) {
+          return false;
+        }
+      }
+      return true;
+    }
+    function clearConsole () {
+      echo chr(27).chr(91).'H'.chr(27).chr(91).'J';
+    }
+    function displayStats () {
+      $this->clearConsole();
+      echo "File is being updated\n";
+      echo "Total Lines:\t" . $this->totalLinesWritten . "\n";
+      echo "File Size:\t" . $this->formatSizeUnits($this->totalBytesWritten) . "\n";
     }
     function make () {
       $fileHandler = fopen($this->file, 'rw+');
-      while (true) {
+      while ($this->keepWriting()) {
         $row = $this->row() . PHP_EOL;
-        $totalBytesWritten += strlen($row);
-        $this->fileSize($this->totalBytesWritten);
+        $this->totalBytesWritten += strlen($row);
+        $this->totalLinesWritten += 1;
         fwrite($fileHandler, $row);
+        $this->displayStats();
       }
       fclose($fileHandler);
+      $this->displayStats();
     }
   }
 
-  $dm = new DataMaker('./db.sdb', '1', 'MB');
+  $dm = new DataMaker('./data-1gb.sdb', '1', 'gb');
   $dm->make();
 
   echo "\n";
